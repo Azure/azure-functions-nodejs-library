d2a7c46a80d66049d3f7c3f33083df6cceb3ba92
Eric Jizba <erijiz@microsoft.com>
2022-08-22 11:41:47 -0700

    Change minimum TypeScript version to v4

--- test/Types.test.ts
+++ test/Types.test.ts
@@ -8,7 +8,7 @@ import { Context } from 'mocha';
 import * as path from 'path';
 
 describe('Public TypeScript types', () => {
-    for (const tsVersion of ['3', '4']) {
+    for (const tsVersion of ['4']) {
         it(`builds with TypeScript v${tsVersion}`, async function (this: Context) {
             this.timeout(10 * 1000);
             expect(await runTsBuild(tsVersion)).to.equal(0);
