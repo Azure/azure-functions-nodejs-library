ea3e14320fa2a4512f8e3a203ebcbb31beca6234
Eric Jizba <erijiz@microsoft.com>
2022-12-05 12:50:54 -0800

    Replace our form support with undici's [v4.x]

--- src/constants.ts
+++ src/constants.ts
@@ -3,16 +3,4 @@
 
 export const version = '4.0.0-alpha.5';
 
-export enum HeaderName {
-    contentType = 'content-type',
-    contentDisposition = 'content-disposition',
-}
-
-export enum MediaType {
-    multipartForm = 'multipart/form-data',
-    urlEncodedForm = 'application/x-www-form-urlencoded',
-    octetStream = 'application/octet-stream',
-    json = 'application/json',
-}
-
 export const returnBindingKey = '$return';
