a46a58de4448fb9794896844e8b977641fe4f9eb
Eric Jizba <erijiz@microsoft.com>
2022-12-09 12:14:44 -0800

    Add `HttpResponse` class and model it after undici

--- src/converters/toRpcHttp.ts
+++ src/converters/toRpcHttp.ts
@@ -1,14 +1,13 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License.
 
-import { HttpResponse } from '@azure/functions';
 import { RpcHttpData, RpcTypedData } from '@azure/functions-core';
-import { Headers } from 'undici';
 import { AzFuncSystemError } from '../errors';
+import { HttpResponse } from '../http/HttpResponse';
 import { toRpcHttpCookie } from './toRpcHttpCookie';
 import { toRpcTypedData } from './toRpcTypedData';
 
-export function toRpcHttp(data: unknown): RpcTypedData | null | undefined {
+export async function toRpcHttp(data: unknown): Promise<RpcTypedData | null | undefined> {
     if (data === null || data === undefined) {
         return data;
     } else if (typeof data !== 'object') {
@@ -16,32 +15,25 @@ export function toRpcHttp(data: unknown): RpcTypedData | null | undefined {
             'The HTTP response must be an object with optional properties "body", "status", "headers", and "cookies".'
         );
     }
-    const response: HttpResponse = data;
 
+    const response = data instanceof HttpResponse ? data : new HttpResponse(data);
     const rpcResponse: RpcHttpData = {};
-    rpcResponse.body = toRpcTypedData(response.body);
-    if (response.status !== null && response.status !== undefined) {
-        if (typeof response.status !== 'string' && typeof response.status !== 'number') {
-            throw new AzFuncSystemError('The HTTP response "status" property must be of type "number" or "string".');
-        } else {
-            rpcResponse.statusCode = response.status.toString();
-        }
-    }
+    rpcResponse.statusCode = response.status.toString();
 
     rpcResponse.headers = {};
-    if (response.headers !== null && response.headers !== undefined) {
-        const headers = new Headers(response.headers);
-        for (const [key, value] of headers.entries()) {
-            rpcResponse.headers[key] = value;
-        }
+    for (const [key, value] of response.headers.entries()) {
+        rpcResponse.headers[key] = value;
     }
 
     rpcResponse.cookies = [];
-    if (response.cookies !== null && response.cookies !== undefined) {
-        for (const cookie of response.cookies) {
-            rpcResponse.cookies.push(toRpcHttpCookie(cookie));
-        }
+    for (const cookie of response.cookies) {
+        rpcResponse.cookies.push(toRpcHttpCookie(cookie));
     }
+
+    rpcResponse.enableContentNegotiation = response.enableContentNegotiation;
+
+    const bodyBytes = await response.arrayBuffer();
+    rpcResponse.body = toRpcTypedData(bodyBytes);
 
     return { http: rpcResponse };
 }
