98bfb9235728a528bcbde297bb16ca5ecf880a49
Eric Jizba <erijiz@microsoft.com>
2023-02-09 13:33:34 -0800

    Fix bug for durable when returning value without a binding set (#60)

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License.
 
-export const version = '4.0.0-alpha.7';
+export const version = '4.0.0-alpha.8';
 
 export const returnBindingKey = '$return';
