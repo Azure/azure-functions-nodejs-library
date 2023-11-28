c7c873d2e8bbf49e4093878d88dd40cc00b54449
Daniel Castro <castro.daniel@microsoft.com>
2023-11-28 11:54:29 -0800

    Switch any to unknown

--- src/converters/toCamelCase.ts
+++ src/converters/toCamelCase.ts
@@ -7,7 +7,7 @@ export function toCamelCaseValue(data: unknown): unknown {
     } else if (Array.isArray(data)) {
         return data.map(toCamelCaseValue);
     } else {
-        const result: Record<string, any> = {};
+        const result: Record<string, unknown> = {};
         for (const [key, value] of Object.entries(data)) {
             result[toCamelCaseKey(key)] = toCamelCaseValue(value);
         }
