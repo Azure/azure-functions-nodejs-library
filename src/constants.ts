6a1e0a88a25a7fd37f63a1ca9bf0d1a5e4f09e6a
Eric Jizba <erijiz@microsoft.com>
2024-01-30 11:22:27 -0800

    Bump version

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License.
 
-export const version = '4.1.0';
+export const version = '4.2.0';
 
 export const returnBindingKey = '$return';
