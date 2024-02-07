de87cbe25e05af90c2109c63e1e790dd252ebb18
Eric Jizba <erijiz@microsoft.com>
2024-02-06 16:32:49 -0800

    Fix conflicting formatting by using prettier for everything (#218)
    * Make prettier the default formatter for all files
    
    * Run prettier

--- webpack.config.js
+++ webpack.config.js
@@ -2,44 +2,42 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = (_env, argv) => {
-    const isDevMode = argv.mode === "development";
+    const isDevMode = argv.mode === 'development';
     return {
-        entry: "./src/index.ts",
+        entry: './src/index.ts',
         target: 'node',
         node: {
-            __dirname: false
+            __dirname: false,
         },
         devtool: 'source-map',
-        externals: [
-            /^[^\.]+/
-        ],
+        externals: [/^[^\.]+/],
         module: {
             parser: {
                 javascript: {
-                    commonjsMagicComments: true
+                    commonjsMagicComments: true,
                 },
             },
             rules: [
                 {
                     test: /\.tsx?$/,
-                    loader: 'ts-loader'
-                }
-            ]
+                    loader: 'ts-loader',
+                },
+            ],
         },
         resolve: {
-            extensions: ['.ts', '.tsx', '.js']
+            extensions: ['.ts', '.tsx', '.js'],
         },
         output: {
             path: `${__dirname}/dist/`,
-            filename: isDevMode ? "azure-functions.js" : "azure-functions.min.js",
-            libraryTarget: "commonjs2"
+            filename: isDevMode ? 'azure-functions.js' : 'azure-functions.min.js',
+            libraryTarget: 'commonjs2',
         },
         plugins: [
             new ForkTsCheckerWebpackPlugin({}),
             new ESLintPlugin({
                 files: ['src/**/*.ts', 'test/**/*.ts'],
-                fix: isDevMode
-            })
-        ]
+                fix: isDevMode,
+            }),
+        ],
     };
-}
+};
