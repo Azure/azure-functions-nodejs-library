d488b48440cfff0e2e0de01430d87a2e042d1cef
Eric Jizba <erijiz@microsoft.com>
2024-02-20 10:37:07 -0800

    Add aka.ms link to stream flag (#222)

--- types/setup.d.ts
+++ types/setup.d.ts
@@ -3,7 +3,8 @@
 
 export interface SetupOptions {
     /**
-     * PREVIEW: Stream http requests and responses instead of loading entire body in memory
+     * PREVIEW: Stream http requests and responses instead of loading entire body in memory.
+     * [Learn more here](https://aka.ms/AzFuncNodeHttpStreams)
      */
     enableHttpStream?: boolean;
 }
