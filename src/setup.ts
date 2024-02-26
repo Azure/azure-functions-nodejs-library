7869a20f3f0b1b803af92e552af89fe07d0441d6
Eric Jizba <erijiz@microsoft.com>
2024-02-26 15:59:04 -0800

    Improve error message for streaming on old host version

--- src/setup.ts
+++ src/setup.ts
@@ -3,6 +3,7 @@
 
 import { SetupOptions } from '../types';
 import { AzFuncSystemError } from './errors';
+import { tryGetCoreApiLazy } from './utils/tryGetCoreApiLazy';
 import { workerSystemLog } from './utils/workerSystemLog';
 
 let options: SetupOptions = {};
@@ -16,6 +17,16 @@ export function setup(opts: SetupOptions): void {
     if (setupLocked) {
         throw new AzFuncSystemError("Setup options can't be changed after app startup has finished.");
     }
+
+    if (opts.enableHttpStream) {
+        // NOTE: coreApi.log was coincidentally added the same time as http streaming,
+        // so we can use that to validate the host version instead of messing with semver parsing
+        const coreApi = tryGetCoreApiLazy();
+        if (coreApi && !coreApi.log) {
+            throw new AzFuncSystemError(`HTTP streaming requires Azure Functions Host v4.28 or higher.`);
+        }
+    }
+
     options = opts;
     workerSystemLog('information', `Setup options: ${JSON.stringify(options)}`);
 }
