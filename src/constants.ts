fa5c4b927c8ffa5d1f856c5f2e83488892bdeced
Victoria Hall <victoria.hall@microsoft.com>
2024-10-29 10:20:59 -0500

    update node library version

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License.
 
-export const version = '4.5.1';
+export const version = '4.6.0';
 
 export const returnBindingKey = '$return';
