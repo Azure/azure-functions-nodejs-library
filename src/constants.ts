a69fe36cc0febb8f3d5b450ff08eb339f6c344d2
hallvictoria <victoriahall@microsoft.com>
2025-02-03 16:10:11 -0600

    update version to 4.6.1

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License.
 
-export const version = '4.6.0';
+export const version = '4.6.1';
 
 export const returnBindingKey = '$return';
