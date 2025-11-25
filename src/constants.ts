70bcdf41e8f7319c0ec16a84472d6fd38582fcfc
swapnil-nagar <swapnilnagar@microsoft.com>
2025-11-25 10:53:31 -0800

    Upgrading to Typescript 5 (#394)

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License.
 
-export const version = '4.9.0';
+export const version = '4.10.0';
 
 export const returnBindingKey = '$return';
