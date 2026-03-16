a87c5c72a865e63456366fab1e663d7a7d08253c
Swapnil Nagar <swapnilnagar@microsoft.com>
2026-03-16 10:47:41 -0700

    Updating the version for major release

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License.
 
-export const version = '4.12.0-preview.1';
+export const version = '4.12.0';
 
 export const returnBindingKey = '$return';
