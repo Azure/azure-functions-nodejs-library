f63233f766676f6bc20a96083d347a1b08e777ac
Tsuyoshi Ushio <tsushi@microsoft.com>
2026-06-16 20:35:51 -0700

    Release @azure/functions 4.16.1

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License.
 
-export const version = '4.16.0';
+export const version = '4.16.1';
 
 export const returnBindingKey = '$return';
